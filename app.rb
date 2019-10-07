5936eacc8fc6a627282edada2cc1d7c328b548b0
Daniel Park <parkdk96@gmail.com>
2019-10-06 17:44:35 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,9 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get ('/name') {'My name is Daniel'}
+  get ('/hometown') {'My hometown is Honolulu'}
+  get ('/favorite-song') {'My favorite song is \'Return to Serenity\''}
+
 end

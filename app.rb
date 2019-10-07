49cd5e725d8a7391d76866038365c56843ee64e0
Soumya_Goyal <somyagoyal739@gmail.com>
2019-10-07 16:01:01 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, World"
+    end
+
+    get '/name' do
+        "My name is Soumya"
+    end
+
+    get '/hometown' do
+        "My hometown is Mainpuri"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is senorita"
+    end
 end
